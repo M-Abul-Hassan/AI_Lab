0e142260d6aaf839bfe709c6d01687c5a12bb338
muhammadabulhassan70@gmail.com <muhammadabulhassan70@gmail.com>
2023-10-09 10:28:42 +0500

    danish commit

---
+++ danish.py
@@ -0,0 +1,9 @@
+a = 20
+b = 30
+c = a+b
+print (c)
+print (f"My name is Muhammad Abul Hassan")
+print (f"My name is Muhammad Abul Hassan")
+print (f"My name is Muhammad Abul Hassan")
+print (f"My name is Muhammad Abul Hassan")
+print (f"My name is Muhammad Abul Hassan")
